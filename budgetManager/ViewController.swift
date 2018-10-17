0ebd3c40bdbd5c57b185387c16fd9f0fcfba5f84
sashatop1 <thatsme3101@gmail.com>
2018-10-17 22:16:51 +0500

    files replaced

--- budgetManager/ViewController.swift
+++ budgetManager/ViewController.swift
@@ -8,38 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class PickerViewVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+ 
     
-       @IBOutlet weak var pickerView: UIPickerView!
-    
-    
-    
-    
-    let array = ["Earned", "Car", "Fun", "House", "Planned costs"]
-
+    @IBOutlet weak var moneyTypePicker: UIPickerView!
+    let array = ["Earned", "Car", "House", "Fun", "Saving"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        pickerView.delegate = self
-        pickerView.dataSource = self
+        //zalupa
+        self.view.addSubview(moneyTypePicker)
+        moneyTypePicker.dataSource? = self
+        moneyTypePicker.delegate? = self
     }
-
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return array.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return array[row]
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        return 0
-        }
-    }
+    
+
 
 
 
